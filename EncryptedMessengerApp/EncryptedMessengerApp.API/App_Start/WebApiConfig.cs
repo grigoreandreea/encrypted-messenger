38bf9ed1e688c1daa9a2ca82884d22fa5956f1af
aeleneiroxana <47813871+aeleneiroxana@users.noreply.github.com>
2019-05-28 23:46:57 +0300

    Update WebApiConfig.cs
    Solved serialization bug, which caused api to crash

--- EncryptedMessengerApp/EncryptedMessengerApp.API/App_Start/WebApiConfig.cs
+++ EncryptedMessengerApp/EncryptedMessengerApp.API/App_Start/WebApiConfig.cs
@@ -23,6 +23,11 @@ namespace EncryptedMessengerApp.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling
+     = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling
+                 = Newtonsoft.Json.PreserveReferencesHandling.Objects;
         }
     }
 }
